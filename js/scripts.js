1f33fe8c774bec2ee4752bc61aa0ce2cfd70066f
Juan Manuel <jmgv87191@gmail.com>
2020-09-06 13:25:30 -0600

    primer version de bacomatic

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    //Efecto menu
+
+    $(".menu a").each(function(index,elemento){
+        $(this).css({
+            "top": "-200px"
+        });
+
+        $(this).animate({
+            top: "0"
+        },2000 + (index * 700));
+    })
+
+    //Efecto header
+    if( $(window).width() > 800){
+        $("header .textos").css({
+            opacity: 0,
+            marginTop: "0px"
+        });
+
+        $("header .textos").animate({
+            opacity: 1,
+            marginTop: "-52px"
+        }, 1500);
+
+    }
+    // efecto scroll
+    var acercaDe = $("#acerca-de").offset().top,
+        platillos = $("#platillos").offset().top,
+        galeria = $("#galeria").offset().top,
+        ubicacion = $("#ubicacion").offset().top
+
+
+    $("#btn-acerca-de").on("click",function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: acercaDe - 300
+        },500);
+    });
+    
+    $("#btn-menu").on("click",function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: platillos - 100
+        },500);
+    });
+
+    $("#btn-galeria").on("click",function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: galeria - 50
+        },500);
+    });
+
+    $("#btn-ubicacion").on("click",function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: ubicacion +10
+        },500);
+    });
+
+
+
+});
+
